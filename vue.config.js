8599901b539335632558cc20afc1c492c04d46ee
sugarmouse <tanghao19950911@gmail.com>
2022-04-20 15:39:10 +0800

    删除webpack自动删除svg颜色配置

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
       .include.add(dir).end() // 只处理 icons 目录
       .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
       .use('svgo-loader').loader('svgo-loader')
-      .tap(options => ({...options, plugins: [{removeAttrs:{attrs:'fill'}}]}))
+      // .tap(options => ({...options, plugins: [{removeAttrs:{attrs:'fill'}}]}))
       .end()
     /* */
     config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'),[{plainSprite:true}])
